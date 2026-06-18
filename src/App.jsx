f46e792983175672bbcdddcfca4cf761665f08bf
Swatchary <swathchary@gmail.com>
2026-06-18 21:41:38 +0530

    basic helloworld

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+
+
+
+  return (
+    <div className="display : flex flex-col items-center justify-center ">
+      <h1 className = 'text-violet-500 text-8xl font-bold text-center mt-30'>
+        Hello world!
+      </h1>
+
+      <button className = 'bg-blue-500 hover:bg-lime-600 text-red font-bold py-2 px-4 rounded-full mt-10'>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default App;
